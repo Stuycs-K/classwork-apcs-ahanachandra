4a4b88001cc781096b174fc2056daa79987171b9
Ahana Chandra <ahanac3@nycstudents.net>
2024-09-27 10:08:43 -0400

    added arrayToString method

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,1 +1,16 @@
+public class MyArrays{
+  public static void main(String[] args){
 
+  }
+
+  public static String arrayToString(int[] nums){
+    String final = "[";
+    for (int i = 0; i < nums.length; i++){
+      final += nums[i];
+      if (i < nums.length - 1){
+        final += ", ";
+      }
+    }
+    return final + "]";
+  }
+}
