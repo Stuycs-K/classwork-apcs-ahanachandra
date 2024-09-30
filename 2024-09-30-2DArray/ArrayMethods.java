d6a69cc6a24a95df8af431789c60a0af2dc537b4
Ahana Chandra <ahanac3@nycstudents.net>
2024-09-30 10:14:48 -0400

    added emails and names

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -1,15 +1,21 @@
+//Ahana Chandra achandra60@stuy.edu
+//Arko Chakrabartiroy achakrabartiroy60@stuy.edu
 public class ArrayMethods{
   public static void main(String[] args) {
+    int[][] testArray = {{3, 8, 2}}
 
   }
-  public static String arrayToString(int[] nums){
+  public static String arrToString(int[] ary){
     String result = "[";
-    for (int i = 0; i < nums.length; i++){
-      result += nums[i];
-      if (i < nums.length - 1){
+    for (int i = 0; i < ary.length; i++){
+      result += ary[i];
+      if (i < ary.length - 1){
         result += ", ";
       }
     }
     return result + "]";
+  }
+  public static String arrToString(int[][] ary) {
+
   }
 }
