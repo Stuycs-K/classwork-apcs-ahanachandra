3bb5f4eca98b399e49ef930ebd7ab2e6029a17c8
Ahana Chandra <ahanac3@nycstudents.net>
2024-09-30 04:55:35 -0400

    added returnCopy test cases

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,6 +1,8 @@
 public class MyArrays{
   public static void main(String[] args){
-
+    int[] arr = [1, 2, 3, 4, 5];
+    System.out.println("Is the original array " + arrayToString(arr) + " the same as " + arrayToString(returnCopy(arr)) + (arrayToString(arr) == arrayToString(returnCopy(arr))));
+    System.out.println("Do the values match?" + (arrayToString(arr).equals(arrayToString(returnCopy(arr)))));
   }
 
   public static String arrayToString(int[] nums){
