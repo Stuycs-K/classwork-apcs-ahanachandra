46f3ff13d1162cbed6719559fcb993ee6717da57
Ahana Chandra <ahanac3@nycstudents.net>
2024-09-30 05:05:38 -0400

    wrote returnCopy and edited test cases

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,18 +1,30 @@
 public class MyArrays{
   public static void main(String[] args){
-    int[] arr = [1, 2, 3, 4, 5];
-    System.out.println("Is the original array " + arrayToString(arr) + " the same as " + arrayToString(returnCopy(arr)) + (arrayToString(arr) == arrayToString(returnCopy(arr))));
+    int[] arr = {1, 2, 3, 4, 5};
+    System.out.println("Is the original array " + arrayToString(arr) + " the same as " + arrayToString(returnCopy(arr)) + arr == returnCopy(arr));
     System.out.println("Do the values match?" + (arrayToString(arr).equals(arrayToString(returnCopy(arr)))));
   }
 
   public static String arrayToString(int[] nums){
-    String final = "[";
+    String result = "[";
     for (int i = 0; i < nums.length; i++){
-      final += nums[i];
+      result += nums[i];
       if (i < nums.length - 1){
-        final += ", ";
+        result += ", ";
       }
     }
-    return final + "]";
+    return result + "]";
   }
+
+
+  public static int[] returnCopy(int[]ary){
+    int[] copiedArray = new int[ary.length];
+    for (int i = 0; i < ary.length; i++){
+      copiedArray[i] = ary[i];
+    }
+    return copiedArray;
+  }
+
+
+
 }
