2f9ee485584ca9a202af6f3cd581e5bc125c8d25
Ahana Chandra <ahanac3@nycstudents.net>
2024-10-01 10:00:51 -0400

    added test cases for first two methods

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -2,7 +2,10 @@
 //Arko Chakrabartiroy achakrabartiroy60@stuy.edu
 public class ArrayMethods{
   public static void main(String[] args) {
-    int[][] testArray = {{3, 8, 2}}
+    int[][] testArray = {{3, 8, 2}, {}, {71, 4, 3, 6}, {22, 9}};
+    int[] otherArray = {2, 5, 3, 66};
+    System.out.println(arrToString(otherArray));
+    System.out.println(arrToString(testArray));
 
   }
   public static String arrToString(int[] ary){
