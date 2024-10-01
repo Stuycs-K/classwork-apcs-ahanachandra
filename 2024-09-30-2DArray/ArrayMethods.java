049059645b8804ee2a7c5fc69426be2c192d79f6
Ahana Chandra <ahanac3@nycstudents.net>
2024-10-01 10:54:12 -0400

    wrote last method and test cases

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -4,9 +4,11 @@ public class ArrayMethods{
   public static void main(String[] args) {
     int[][] testArray = {{3, 8, 2}, {}, {71, 4, 3, 6}, {22, 9}};
     int[] otherArray = {2, 5, 3, 66};
+    int[][] rectangularArray = {{4, 2, 89, 2}, {3, 12, 17, 3,}, {1, 2, 3, 4}};
     System.out.println(arrToString(otherArray));
     System.out.println(arrToString(testArray));
     System.out.println(arr2DSum(testArray));
+    System.out.println(swapRC(rectangularArray));
 
   }
   public static String arrToString(int[] ary){
@@ -38,6 +40,16 @@ public class ArrayMethods{
       }
     }
     return sum;
+  }
+
+  public static int[][] swapRC(int[][] nums){
+    int[][] returnedArr = new int[nums[0].length][nums.length];
+    for (int i = 0; i < nums.length; i++){
+      for (int x = 0; x < nums[0].length; x++){
+        returnedArr[x][i] = nums[i][x];
+      }
+    }
+    return returnedArr;
   }
 
 
