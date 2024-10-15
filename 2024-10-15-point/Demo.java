1d523581fbbb3d7129ba56e55c761d9671995baf
Ahana Chandra <ahanac3@nycstudents.net>
2024-10-15 09:53:35 -0400

    wrote Point constructor and tested

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -13,8 +13,8 @@ public class Demo{
     Point p2 = new Point(-1,-1);
     Point p3 = new Point(3,4);
     System.out.println( p3);
-    System.out.println( distance(p1,p2));
-    System.out.println( Point.distance(p1,p2));
-    System.out.println( p1.distanceTo(p2));
+    //System.out.println( distance(p1,p2));
+    //System.out.println( Point.distance(p1,p2));
+    //System.out.println( p1.distanceTo(p2));
   }
 }
--- 2024-10-15-point/Point.java
+++ 2024-10-15-point/Point.java
@@ -7,12 +7,12 @@ public class Point{
 
   //Initialize this Point to have the same values as the other Point.
   public Point(Point other){
-    //COMPLETE THIS SECOND
-    //Then write test cases.
+    this.x = other.getX();
+    this.y = other.getY();
   }
 
   //Initialize this Point to have the provided values
-  public Point(double x, double Y){
+  public Point(double x, double y){
     this.x=x;
     this.y=y;
   }
