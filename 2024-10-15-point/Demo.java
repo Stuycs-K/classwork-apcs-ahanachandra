a43034913105e519458e5adcb3b856fa0b578749
Ahana Chandra <ahanac3@nycstudents.net>
2024-10-16 04:55:01 -0400

    wrote distance method in Point class and tested successfully

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -18,7 +18,7 @@ public class Demo{
     System.out.println(p4);
     System.out.println(p5);
     System.out.println( distance(p1,p2));
-    //System.out.println( Point.distance(p1,p2));
+    System.out.println( Point.distance(p1,p2));
     System.out.println( p1.distanceTo(p2));
   }
 }
