a616562c806317dd89cfc61df33a035cd64bc6d6
Ahana Chandra <ahanac3@nycstudents.net>
2024-10-31 09:49:02 -0400

    added Driver class and the framework for the two examples

---
+++ 10-31-Advent/Driver.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Driver{
+  public static void main(String[] args) {
+      Scanner sc = new Scanner("Some String (or File) Here");
+      String text = "This is a sample\nOf how to read\ntext!";
+
+      // creates a Scanner object to read throug the String
+      Scanner input = new Scanner(text);
+
+      //you don't want a for loop here unless you know the size of the data set
+
+      while(input.hasNext()){
+        System.out.println(input.next());
+      }
+
+      //Print out each word per one line:
+      Scanner sc1 = new Scanner("This is a bunch of words");
+
+      //Calculating the sum of values in this string using nextDouble:
+      Scanner sc2 = new Scanner("10.0 1.04 99.0 -3.0 4.2");
+      }
+    }
