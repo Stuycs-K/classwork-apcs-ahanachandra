d25df3c7d24c5d45cb680b1c99610e847f525d4c
Ahana Chandra <ahanac3@nycstudents.net>
2024-10-31 09:56:38 -0400

    Added the while loops for printing out each word per one line as calculating the sum of values using nextDouble.

--- 10-31-Advent/Driver.java
+++ 10-31-Advent/Driver.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 public class Driver{
   public static void main(String[] args) {
       Scanner sc = new Scanner("Some String (or File) Here");
+
+      System.out.println("\nExample test:");
       String text = "This is a sample\nOf how to read\ntext!";
 
       // creates a Scanner object to read throug the String
@@ -14,9 +16,19 @@ public class Driver{
       }
 
       //Print out each word per one line:
+      System.out.println("\nPrinting out a word from each line:");
       Scanner sc1 = new Scanner("This is a bunch of words");
+      while(sc1.hasNext()){
+        System.out.println(sc1.next());
+      }
 
       //Calculating the sum of values in this string using nextDouble:
+
       Scanner sc2 = new Scanner("10.0 1.04 99.0 -3.0 4.2");
+      double sum = 0;
+      while(sc2.hasNextDouble()){
+          sum = sum + sc2.nextDouble();
       }
+      System.out.println(sum);
     }
+  }
