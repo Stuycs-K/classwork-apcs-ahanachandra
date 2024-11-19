eb4dae33d92a1ba44b1aa4d3ebe0b05c5ef11be6
Ahana Chandra <ahanac3@nycstudents.net>
2024-11-19 10:14:20 -0500

    started the Day1 class, will finish later

---
+++ 2024-11-18-advent2016/Day1.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day1{
+  public static void main(String[] args){
+
+  }
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
