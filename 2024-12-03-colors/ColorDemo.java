e18307fe6dd328de5dcbe1006b2d4ebbb42f3689
Ahana Chandra <ahanac3@nycstudents.net>
2024-12-04 09:38:24 -0500

    pulled changes

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -1,28 +1,16 @@
 public class ColorDemo{
-<<<<<<< HEAD
-  public static void main(String[] args){
-    for(int r = 0; r < 256; r+=32){
-  for(int g = 0; g <= 256; g+=32){
-    for(int b = 0; b <= 256; b+=32){
-      System.out.print("\u001b[38;2;"+r+";"+g+";"+b+";7m.");
-    }
-  }
-    System.out.println();
-}
-  }
-=======
     public static void main(String[] args) {
         for (int x = 1; x < 101; x++){
             for (int i = 1; i < x; i++){
                 if (i % 2 != 0){
                     System.out.print("\u001b[36m Are you ");
-                    System.out.print(i + "?"); 
+                    System.out.print(i + "?");
                 }
                 else{
                     System.out.print("\u001b[35m Are you ");
-                    System.out.print(i + "?"); 
+                    System.out.print(i + "?");
                 }
-    
+
             }
             System.out.print("\u001b[30;47mYou're ");
             System.out.print(x + "!! Happy Birthday!");
@@ -30,7 +18,6 @@ public class ColorDemo{
         }
         System.out.print("\u001b[0m");
     }
->>>>>>> 4217541b9f25aa05b51b5900b67d19890a71bdb6
 
 
 
@@ -40,7 +27,6 @@ public class ColorDemo{
 
 
 
-<<<<<<< HEAD
 
 
 
@@ -48,6 +34,3 @@ public class ColorDemo{
 
 
 }
-=======
-}
->>>>>>> 4217541b9f25aa05b51b5900b67d19890a71bdb6
