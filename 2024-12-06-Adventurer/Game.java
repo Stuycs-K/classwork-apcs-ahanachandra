110ebdf4107d9aea8d3b7cdaa34376e1fe766df1
Ahana Chandra <ahanac3@nycstudents.net>
2024-12-09 10:13:50 -0500

    started Game class

---
+++ 2024-12-06-Adventurer/Game.java
@@ -0,0 +1,12 @@
+public class Game{
+  public static void main(String[] args){
+    Scanner userInput = new Scanner(System.in);
+    Adventurer person = new Fairy();
+    Adventurer enemy = new CodeWarrior();
+    while (userInput.hasNextLine()){
+      System.out.println("Enter username:");
+      System.out.println("Enter HP number");
+      System.out.println("")
+    }
+  }
+}
